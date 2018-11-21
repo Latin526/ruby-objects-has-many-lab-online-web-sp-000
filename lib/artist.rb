8a089c587382a1d5d1a51d17245bd4bf37f5e6ae
Yasmin Torres <yasmintorres90@hotmail.com>
2018-11-21 22:53:31 +0000

    automatically backed up by learn

---
+++ lib/artist.rb
@@ -0,0 +1,26 @@
+require 'pry'
+
+class Artist 
+  attr_accessor :name, :songs
+  
+  @@song_count = 0
+  
+  def initialize(name)
+    @name = name 
+    @songs = []
+  end
+
+   def add_song(song)
+     song = Song.new(song)
+    @songs << song 
+    song.artist = self
+  end
+  
+  def songs
+    @songs 
+  end
+  
+   def add_song_by_name(name)
+    
+  end
+end 
