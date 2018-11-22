37f91557dce00a0059d21298987101f6aa94a9c2
Yasmin Torres <yasmintorres90@hotmail.com>
2018-11-22 00:03:50 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,7 +1,14 @@
-class Song 
-  attr_accessor  :artist, :name, :genre 
-  
-  def initialize(name)
-    @name = name
-  end
+class Song	
+  attr_accessor :name, :artist	
+   @@all = []	
+   def initialize(name)	
+    @name = name	
+    @@all << self	
+  end	
+   def self.all	
+    @@all	
+  end	
+   def artist_name	
+    artist.name if artist	
+  end	
 end
