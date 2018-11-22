3ca406b2614316deb627338477fab79d343cb603
Yasmin Torres <yasmintorres90@hotmail.com>
2018-11-22 00:05:35 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,26 +1,30 @@
-require 'pry'
-
-class Artist 
-  attr_accessor :name, :songs
+class Artist	
+  attr_accessor :name	
+   @@all = []	
+   
+  def initialize(name)	
+    @name = name	
+    @@all << self	
+  end	
   
-  @@song_count = 0
+  def self.all	
+    @@all	
+  end	
   
-  def initialize(name)
-    @name = name 
-    @songs = []
-  end
-
-  def add_song(song)
-    @songs << song
-    song.artist = self
-  end
+  def add_song(song)	
+    song.artist = self	
+  end	
   
-  def songs
-    @songs 
-  end
+  def add_song_by_name(name)	
+    song = Song.new(name)	
+    song.artist = self	
+  end	
   
-   def add_song_by_name(name)
-    
-    song.artist = self
-  end
-end 
+  def songs	
+    Song.all.select {|song| song.artist == self}	
+  end	
+  
+  def self.song_count	
+    Song.all.count	
+  end	
+end
